665c6b93bf221ddbc8587883136be9869c01fa65
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-12-07 14:23:37 +0100

    add opencode zen provider for anthropic

--- exercises/01-ai-sdk-basics/01.04-generating-text/problem/main.ts
+++ exercises/01-ai-sdk-basics/01.04-generating-text/problem/main.ts
@@ -3,12 +3,14 @@ import { generateText } from "ai";
 import { opencodeZen } from "@local/utils";
 import { zenGoogle } from "@local/utils/src/providers/zen-google.ts";
 import { zenOpenai } from "@local/utils/src/providers/zen-openai.ts";
+import { zenAnthropic } from "@local/utils/src/providers/zen-anthropic.ts";
 
 // I recommend using the Google Gemini model:
 // gemini-2.0-flash-lite
 // const model = opencodeZen("big-pickle");
 // const model = zenGoogle("gemini-3-pro");
-const model = zenOpenai("gpt-5-nano");
+// const model = zenOpenai("gpt-5-nano");
+const model = zenAnthropic("claude-haiku-4-5");
 
 const prompt = "What is the capital of France?";
 
